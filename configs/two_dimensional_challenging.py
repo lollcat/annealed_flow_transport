df039ab3257e2df022039f752146dd8aa81e612e
lollcat <laurencemidgley@gmail.com>
2023-02-27 14:42:46 +0000

    feat: update craft to latest version

--- configs/two_dimensional_challenging.py
+++ configs/two_dimensional_challenging.py
@@ -26,24 +26,28 @@ def get_config():
 
   config.seed = 1
   config.batch_size = 2000
+  config.craft_batch_size = 2000
   config.estimation_batch_size = 2000
   config.sample_shape = (num_dim,)
   config.report_step = 1
   config.vi_report_step = 50
-  config.num_temps = 4
+  config.num_temps = 5
   config.step_logging = False
   config.resample_threshold = 0.3
   config.stopping_criterion = 'time'
   config.use_resampling = True
   config.use_markov = True
+  config.use_path_gradient = False
   config.algo = 'craft'
   config.vi_iters = 1000
+  config.craft_num_iters = 1000
   config.vi_estimator = 'importance'
 
   optimization_config = ConfigDict()
   optimization_config.free_energy_iters = 1000
   optimization_config.aft_step_size = 1e-3
   optimization_config.vi_step_size = 1e-3
+  optimization_config.craft_step_size = 1e-2
 
   config.optimization_config = optimization_config
 
@@ -73,11 +77,6 @@ def get_config():
   config.flow_config = flow_config
 
   mcmc_config = ConfigDict()
-  mcmc_config.rwm_steps_per_iter = 3
-  rw_step_config = ConfigDict()
-  rw_step_config.step_sizes = [0.5, 0.5, 0.5, 0.3]
-  rw_step_config.step_times = [0., 0.25, 0.5, 1.]
-  mcmc_config.rwm_step_config = rw_step_config
 
   hmc_step_config = ConfigDict()
   hmc_step_config.step_times = [0., 0.25, 0.5, 1.]
@@ -88,6 +87,7 @@ def get_config():
   nuts_step_config.step_sizes = [2.0, 2.0, 2.0, 2.0]
   mcmc_config.hmc_steps_per_iter = 10
   mcmc_config.hmc_num_leapfrog_steps = 10
+  mcmc_config.rwm_steps_per_iter = 0
 
   mcmc_config.slice_steps_per_iter = 0
   mcmc_config.nuts_steps_per_iter = 0
@@ -98,10 +98,5 @@ def get_config():
   initial_sampler_config = ConfigDict()
   initial_sampler_config.initial_sampler = 'MultivariateNormalDistribution'
   config.initial_sampler_config = initial_sampler_config
-
-
-  config.craft_num_iters = 1000
-  config.craft_batch_size = 2000
-  optimization_config.craft_step_size = 1e-2
 
   return config
