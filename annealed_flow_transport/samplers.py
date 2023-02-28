feat: fix scaling bug, add params_filename

annealed_flow_transport/samplers.py
@@ -45,8 +45,9 @@ class MultivariateNormalDistribution(object):
 
   def __init__(self, config):
     self._config = config
+    self.diagonal_cov = config.diagonal_cov if config.diagonal_cov else 1.
 
   def __call__(self, key: RandomKey, num_samples: int,
                sample_shape: Tuple[int]) -> Array:
     batched_sample_shape = (num_samples,) + sample_shape
-    return jax.random.normal(key, shape=batched_sample_shape) * jnp.sqrt(self._config.diagonal_cov)
+    return jax.random.normal(key, shape=batched_sample_shape) * jnp.sqrt(self.diagonal_cov)